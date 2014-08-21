2dc013300c4b0fb605fa9dd2a3fba5ecc81ac20c
Kenneth Reitz <me@kennethreitz.com>
2014-08-21 13:41:34 -0400

    v0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='pip-pop',
-    version='0.0.0',
+    version='0.1.0',
     url='https://github.com/kennethreitz/pip-pop',
     license='MIT',
     author='Kenneth Reitz',
