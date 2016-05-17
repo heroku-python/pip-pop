99d9f36ad765535946af1fa9fc181d33668ee146
Ed Morley <emorley@mozilla.com>
2016-05-17 17:52:21 +0100

    Require pip>=1.5.0
    Since anything older fails with:
      ImportError: No module named _vendor.requests

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     #include_package_data=True,
     zip_safe=False,
     platforms='any',
-    install_requires=['docopt'],
+    install_requires=['docopt', 'pip>=1.5.0'],
     classifiers=[
         # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
         #'Development Status :: 1 - Planning',
